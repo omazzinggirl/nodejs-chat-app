f7bd5a5c456130fdbcd645a448dc0ad4949747ce
oisheemaz <oishee.sde@gmail.com>
2021-10-26 17:06:36 +0530

    Added message and location sharing functionalities

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,31 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#messageForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (message) => {
+        console.log('Message status ::: ', message)
+    })
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        // console.log(position)
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared')
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app  = express()
 const server = http.createServer(app)
@@ -12,10 +13,34 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New web connection')
+    
+    socket.emit('message', "Welcome!")
+
+    socket.broadcast.emit('message', 'A new user has joined!')
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Error')
+        }
+        io.emit('message', message)
+        callback('Delivered')
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', `Location: ${coords.latitude}, ${coords.longitude}`)
+        io.emit('message', `https://google.com/maps?${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left...')
+    })
 })
 
-server.listen(port, () => {
-    console.log(`Server is running on `+ port)
+server.listen(port, () => { 
+    console.log(`Server is running on ${port}!`)
 })
